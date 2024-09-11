8b38f7f829d99390bc51c5cf4caaab709326e7fe
Henry Lau <henry_lau77@hotmail.com>
2024-09-11 13:48:30 -0700

    testing

--- app/src/main/java/us/myapplication/data/DataViewModel.kt
+++ app/src/main/java/us/myapplication/data/DataViewModel.kt
@@ -1,13 +1,10 @@
 package us.myapplication.data
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -19,6 +16,7 @@ class DataViewModel(
 
     private val TIMEOUT_MILLIS = 5_000L
     private var _stocksState = MutableStateFlow(StocksUiState(isLoading = true))
+
     //https://medium.com/@mortitech/sharein-vs-statein-in-kotlin-flows-when-to-use-each-1a19bd187553
     //val uiState: StateFlow<StocksUiState> = _stocksState.asStateFlow()
     val uiState: StateFlow<StocksUiState> = _stocksState.stateIn(
